3c41577942978876f5bb31b61f7139badbd86977
unknown <412588801@qq.com>
2024-03-12 16:44:18 +0800

    1

--- Demo1/src/Item.tsx
+++ Demo1/src/Item.tsx
@@ -1,10 +1,10 @@
 import React from "./React";
-export default function(props){
-  let func = (e)=>{
+export default function (props) {
+  let func = (e) => {
     let target = e.currentTarget as HTMLInputElement;
     alert(target.innerText);
   }
-    return (
-      <li onClick={func}>Hello：{props.data}</li>
-    );
+  return (
+    <li onClick={func}>Hello：{props.data}-{props.children}</li>
+  );
 };
--- Demo1/src/index.tsx
+++ Demo1/src/index.tsx
@@ -11,7 +11,7 @@ let App = () => {
     <div>这是一个列表： </div>
     <ul>
       {arr.map((v, i) => (
-        <Item data={v}></Item>
+        <Item data={v}>{i + 1}</Item>
       ))}
     </ul>
     <div onClick={func}>增加一个</div>
@@ -20,3 +20,15 @@ let App = () => {
 document.addEventListener("DOMContentLoaded", () => {
   document.body.appendChild(<App></App>);
 });
+
+
+
+// let container = document.getElementById("container");
+// for(let i=0;i<6;i++){
+//     let row = <div class="row"></div>
+//     for(let j=0;j<7;j++){
+//         let cell = <div><div class="cellHeader">{obj.content}</div></div>
+//         row.appendChild(cell)
+//     }
+//     container.append(row)
+// }
